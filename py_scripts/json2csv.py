409fac761beab017fc93eeb955a3b0a7eb2b8565
João Neto <joaomlneto@ist.utl.pt>
2014-12-09 14:44:53 +0100

    print ALL

--- py_scripts/json2csv.py
+++ py_scripts/json2csv.py
@@ -13,13 +13,13 @@ print "emitted, transferred, completeLatency, acked, failed"
 while True:
     response = urllib2.urlopen(topology_string)
     data = json.load(response)
-    result_string = ""
-    result_string = str(data["topologyStats"][3]["emitted"]) + ", "
-    result_string += str(data["topologyStats"][3]["transferred"]) + ", "
-    result_string += data["topologyStats"][3]["completeLatency"] +", "
-    result_string += str(data["topologyStats"][3]["acked"]) +", "
-    result_string += str(data["topologyStats"][3]["failed"])
-    print result_string
+#    result_string = ""
+#    result_string = str(data["topologyStats"][3]["emitted"]) + ", "
+#    result_string += str(data["topologyStats"][3]["transferred"]) + ", "
+#    result_string += data["topologyStats"][3]["completeLatency"] +", "
+#    result_string += str(data["topologyStats"][3]["acked"]) +", "
+#    result_string += str(data["topologyStats"][3]["failed"])
+    print data
     time.sleep(sleep_t)
 
 
