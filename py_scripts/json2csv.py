81cc4ada5974e442ee1da6af5218974b93e2de07
Seckin Savasci <seckin@savasci.org>
2014-12-09 14:08:24 +0100

    py2 working version

--- py_scripts/json2csv.py
+++ py_scripts/json2csv.py
@@ -4,12 +4,14 @@ import json
 import sys
 
 
-storm_ui_url = sys.argv[1]
+storm_ui_url = "http://"+sys.argv[1]
 topology_string =  storm_ui_url+"/api/v1/topology/"+ sys.argv[2]
 sleep_t = int(sys.argv[3])
 
+print "emitted, transferred, completeLatency, acked, failed"
+
 while True:
-    response = urllib3.urlopen(topology_string)
+    response = urllib2.urlopen(topology_string)
     data = json.load(response)
     result_string = ""
     result_string = str(data["topologyStats"][3]["emitted"]) + ", "
